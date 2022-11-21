399a96ff6ea70f027c6932132bb4430808ff1df7
Daniel Radvanyi <radvanyi.daniel@gmail.com>
2022-11-21 20:58:05 +0200

    added registering, login and sessions

--- app.js
+++ app.js
@@ -2,22 +2,36 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const sessions = require('express-session')
 const app = express()
 require('dotenv').config()
-
-app.use(express.static('./public'))
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(express.static(__dirname));
 
 // Connect to MongoDB database
 // create a .env variable called DATABASE with the DB URI as value
 mongoose.connect(process.env.DATABASE, () => {
 }, e => console.error(e))
+const User = require('./models/Users');
+const FeederSetup = require("./models/Feeder-setup")
 
-var router = require('./routes/router');
+// Sessions
+const session_length = 1800000 // half an hour
+app.use(sessions({
+    secret: "extremelyv3rymassivleyultrasecuresecretcode",
+    saveUninitialized:true,
+    cookie: { maxAge: session_length },
+    resave: false 
+}));
+
+// Router
+const router = require('./routes/router');
 app.use('/', router);
-//GET to homepage
 
 // Run server
 app.listen(process.env.PORT, () => {
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,112 @@
 module.exports = (function() {
     'use strict';
-    var router = require('express').Router();
+    var router = require('express').Router()
+    const crypto = require('crypto')
+    const User = require('../models/Users')
+    
+    var session
 
+
+    // GET main page
     router.get("/", (req, res) => {
-        res.render("home.ejs");
+        session=req.session;
+        if(session.userid){ // if there is an active session with userid
+            res.send("Hey there, welcome "+login+". <a href=\'/logout'>click to logout</a>")
+        }else{
+            res.render("home.ejs")
+        }
+    });
+
+    // POST login form
+    router.post('/login', async (req,res) => {
+        var login = await Authorizer(req.body.username, req.body.password);
+
+        if(login != null){
+            session=req.session
+            session.userid=login
+            console.log("Session: "+req.session)
+            res.send("Hey there, welcome "+login+". <a href=\'/logout'>click to logout</a>")
+        }else{
+            res.send('Invalid username or password')
+        }
+    })
+
+    // log out of session
+    router.get('/logout',(req,res) => {
+        req.session.destroy();
+        res.redirect('/');
+    });
+
+    // GET register page
+    router.get("/register", (req, res) => {
+        res.render("register.ejs")
+    });
+
+    /* POST register form */
+    router.post("/register", async (req, res) => {
+        crypto.pbkdf2(req.body.password, "saltysalt", 200000, 64, "sha512", async (err, pbkdf2Key) => {
+            if (err) throw err;
+            if (!(await checkUsername(req.body.username))) { //check if username is taken
+            const response = await User.create({
+                username: req.body.username,
+                password: pbkdf2Key.toString("hex")
+            });
+            console.log("User created successfully: ", response);
+            res.redirect("/"); //redirect to homepage
+            }else{
+                res.send("Username already taken. <a href=\'/register'>click to register again</a>");
+            }
+        });
     });
 
 
+    // Functions
+
+    /**
+     * @function Authorizer
+     * @parameters recieves the username and password from login form
+     * @description finds if username with given password exists in database
+     * @return username or null
+     **/
+    async function Authorizer(username, password) {
+        var returnable;
+
+        const key = crypto.pbkdf2Sync(password, "saltysalt", 200000, 64, "sha512") // hash password to encrypted version
+        const userQuery = await User.findOne({
+            username: username,
+            password: key.toString("hex"),
+        }).then((user) => {
+            if (user) {
+                returnable = username;
+            } else {
+                returnable = null;
+            }
+        }).catch((err) => {
+            returnable = null; //error
+        });
+        return returnable;
+    }
+
+    /**
+     * @function checkUsername
+     * @description Checks whether the username is taken or not
+     * @return {object} returns if username is taken(true) or not(false)
+     **/
+    async function checkUsername(username) {
+        let taken = false;
+        await User.findOne({ username: username }).then(user => {
+            if (user) {
+                taken = true;
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+        return taken;
+    }
+
+    
     return router;
 })();
+
