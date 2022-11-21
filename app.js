20feaa6d00704e89015f51a143919c9855dd4f8d
Ha GIS <khanhhado1208@gmail.com>
2022-11-21 23:02:48 +0200

    changes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Module requires
 const express = require('express')
 const bodyParser = require("body-parser")
+const sessions = require('express-session')
+const passport = require('passport');
+const path = require('path')
+const app = express() /* Init express app */
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const sessions = require('express-session')
-const app = express()
 require('dotenv').config()
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -14,7 +17,6 @@ app.use(cookieParser());
 app.use(express.static(__dirname));
 
 // Connect to MongoDB database
-// create a .env variable called DATABASE with the DB URI as value
 mongoose.connect(process.env.DATABASE, () => {
 }, e => console.error(e))
 const User = require('./models/Users');
@@ -29,11 +31,30 @@ app.use(sessions({
     resave: false 
 }));
 
-// Router
-const router = require('./routes/router');
-app.use('/', router);
+/* Parse JSON Data */
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-// Run server
+/* Set Cookie Parser, Sessions */
+const session_length = 1800000 // half an hour
+app.use(sessions({
+    secret: "extremelyv3rymassivleyultrasecuresecretcode",
+    saveUninitialized:true,
+    cookie: { maxAge: session_length },
+    resave: false 
+}));
+app.use(cookieParser());
+
+/* Utilizing ejs template */
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+
+/* Handling routes request */
+app.use('/', router) /* Access to homepage */
+app.use('/register', router) /* Access to register page */
+
+/* Start the server */
 app.listen(process.env.PORT, () => {
     console.log("Port " + process.env.PORT+" open");
 });
